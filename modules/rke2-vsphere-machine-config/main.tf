77f3f0fd8f45cd0ff69d9c43114142877b7cede5
Armagan Karatosun <armagankaratosun@gmail.com>
2023-08-24 17:53:46 +0200

    added new module "rke2-vsphere-machine-config"

---
+++ modules/rke2-vsphere-machine-config/main.tf
@@ -0,0 +1,37 @@
+data "rancher2_cloud_credential" "cloud_credential_name" {
+    name = var.cloud_credential_name
+}
+
+# Create openstack machine config v2
+resource "rancher2_machine_config_v2" "machine" {
+  generate_name              = var.machine_config_name
+  vsphere_config {
+    datacenter               = var.vsphere_datacenter
+    datastore                = var.vsphere_datastore
+    boot2docker_url          = var.vsphere_boot2docker_url
+    clone_from               = var.vsphere_virtual_machine_template
+    cfgparam                 = var.vsphere_cfgparam
+    cloud_config             = var.cloud_init_data
+    creation_type            = var.vsphere_virtual_machine_creation_type
+    folder                   = var.vsphere_folder
+    pool                     = var.vsphere_resource_pool
+    
+    cpu_count                = var.vm_num_cpus
+    disk_size                = var.vm_disk_size
+    memory_size              = var.vm_memory
+    network                  = var.vm_network
+#    ssh_user                 = var.ssh_user
+#    ssh_user_group           = var.ssh_user_group
+#    ssh_port                 = var.ssh_port
+#    ssh_password             = var.ssh_password
+  }
+}
+
+output "machine_config_kind" {
+  value = rancher2_machine_config_v2.machine.kind
+}
+output "machine_config_name" {
+  value = rancher2_machine_config_v2.machine.name
+}
+
+
---
+++ modules/rke2-vsphere-machine-config/variables.tf
@@ -0,0 +1,101 @@
+variable "machine_config_name" {
+  description = "The generated name for the machine config"
+  default = "node-config"
+  type = string
+}
+
+variable "vsphere_datacenter" {
+  description = "Name of the vSphere datacenter (Optional)"
+  default = ""
+  type = string
+}
+
+variable "vsphere_datastore" {
+  description = "Name of the vSphere datastore (Optional)"
+  default = ""
+  type = string
+}
+
+variable "vsphere_resource_pool" {
+  description = "Name of the vSphere resource pool (Optional)"
+  default = ""
+  type = string
+}
+
+variable "vsphere_boot2docker_url" {
+  description = "URL for the boot2docker ISO (Optional)"
+  default = ""
+  type = string
+}
+
+variable "vsphere_cfgparam" {
+  description = "vSphere vm configuration parameters (used for guestinfo) (Optional)"
+  default = ["disk.enableUUID=TRUE"]
+  type = list
+}
+
+variable "vsphere_virtual_machine_template" {
+  description = "Name of the vSphere virtual machine template to clone from (Optional)"
+  default = ""
+  type = string
+}
+
+variable "cloud_init_data" {
+  description = "Relative Path for Cloud-init configuration yaml file (Optional)"
+  default = ""
+  type = string
+}
+
+variable "vsphere_virtual_machine_creation_type" {
+  description = "Creation type when creating a new virtual machine. Supported values: vm, template, library, legacy. Defaults to 'template' (Optional)"
+  default = "template"
+  type = string
+}
+
+variable "vsphere_folder" {
+  description = "Name of the vSphere folder for the nodes. This folder must already exist in the datacenter (Optional)"
+  default = ""
+}
+
+variable "vm_num_cpus" {
+  description = "Number of virtual CPUs for the VM/nodes. Defaults to '2'"
+  default = "2"
+  type = string
+}
+
+variable "vm_disk_size" {
+  description = "Size of the VM's disk in MiB. Defaults to '30720'"
+  default = "30720"
+  type = string
+}
+
+variable "vm_memory" {
+  description = "Size of the VM's memory in MiB. Defaults to '4096'"
+  default = "4096"
+  type = string
+}
+
+variable "vm_network" {
+  description = "Name of the VM's network"
+  type = list
+}
+
+variable "ssh_user" {
+  description = "SSH user for the VM"
+  default = "docker"
+}
+
+variable "ssh_user_group" {
+  description = "SSH user group for the VM"
+  default = "staff"
+}
+
+variable "ssh_port" {
+  description = "SSH port for the VM"
+  default = 22
+}
+
+variable "ssh_password" {
+  description = "SSH password for the VM"
+  default = "tcuser"
+}
