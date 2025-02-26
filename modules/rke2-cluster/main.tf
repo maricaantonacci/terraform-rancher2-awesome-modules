5edb2040a0e2d0aa9c2c793b1b7c79de71a785d0
Armagan Karatosun <armagankaratosun@gmail.com>
2025-02-26 11:44:39 +0100

    moving outputs out from main.tf

--- modules/rke2-cluster/main.tf
+++ modules/rke2-cluster/main.tf
@@ -43,15 +43,3 @@ resource "rancher2_cluster_v2" "rke2_cluster" {
     }
   }
 }
-
-output "rancher_cluster_id" {
-  value = rancher2_cluster_v2.rke2_cluster.id
-}
-
-output "cluster_name" {
-  value = rancher2_cluster_v2.rke2_cluster.name
-}
-
-output "kubeconfig" {
-  value = rancher2_cluster_v2.rke2_cluster.kube_config
-}
