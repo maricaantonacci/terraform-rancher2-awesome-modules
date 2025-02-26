da4c3481d50bcbb396138998759e00ceaae016c1
Armagan Karatosun <armagankaratosun@gmail.com>
2025-02-26 11:46:29 +0100

    Update outputs.tf

--- modules/rke2-cluster/outputs.tf
+++ modules/rke2-cluster/outputs.tf
@@ -10,7 +10,3 @@ output "kubeconfig" {
   value = rancher2_cluster_v2.rke2_cluster.kube_config
 }
 
-output "nodepool_instance_ids" {
-  value = [for np in rancher2_cluster_v2.rke2_cluster.nodepools : np.id]
-  description = "List of instance IDs for each nodepool"
-}
