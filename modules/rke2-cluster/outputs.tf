6854da86c35e47d8de7df8a07ab4b5a3846a1469
Marica Antonacci <marica.antonacci@ecmwf.int>
2025-03-16 17:16:01 +0100

    Add output cluster_v1_id

--- modules/rke2-cluster/outputs.tf
+++ modules/rke2-cluster/outputs.tf
@@ -2,6 +2,10 @@ output "rancher_cluster_id" {
   value = rancher2_cluster_v2.rke2_cluster.id
 }
 
+output "rancher_cluster_v1_id" {
+  value = rancher2_cluster_v2.rke2_cluster.cluster_v1_id
+}
+
 output "cluster_name" {
   value = rancher2_cluster_v2.rke2_cluster.name
 }
